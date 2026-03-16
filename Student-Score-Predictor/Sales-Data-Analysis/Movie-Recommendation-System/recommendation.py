f40cc8501824c7f4c10556b4c8f7d4055689246e
MonaMonalisa-Behera <bmonalisa8847@gmail.com>
2026-03-16 22:58:19 +0530

    Delete Student-Score-Predictor/Sales-Data-Analysis directory

--- Student-Score-Predictor/Sales-Data-Analysis/Movie-Recommendation-System/recommendation.py
+++
@@ -1,9 +0,0 @@
-movies = ["Avatar", "Titanic", "Avengers", "Inception"]
-
-user_choice = "Avatar"
-
-print("Because you watched:", user_choice)
-
-for movie in movies:
-    if movie != user_choice:
-        print("Recommended:", movie)
